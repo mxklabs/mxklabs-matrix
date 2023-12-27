225f0bc21fdbc71074cff558c813d4a0b330114f
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2023-12-27 11:12:26 +0000

    Add stream functionality

--- desktopgui/client.py
+++ desktopgui/client.py
@@ -9,13 +9,11 @@ import sys
 import threading
 import time
 
-from PIL import ImageGrab, Image
+from PIL import ImageGrab, Image, ImageChops
 
 from screengrab import main as screengrab
 
 from PyQt6 import QtCore, QtGui, QtWidgets, uic
-
-import clientapi
 
 Resolution = collections.namedtuple("Resolution", ["width", "height"])
 
@@ -34,16 +32,16 @@ def PIL_to_qimage(pil_img):
         QtGui.QImage.Format.Format_RGBA8888
     )
 
-class DesktopApp(QtWidgets.QMainWindow):
+class ClientApp(QtWidgets.QMainWindow):
   _screen_preview_thread_fired = QtCore.pyqtSignal(name="previewThreadFired")
 
-  def __init__(self):
+  def __init__(self, client_handler):
     QtWidgets.QMainWindow.__init__(self, parent=None)
-    self._window = uic.loadUi(pathlib.Path(__file__).parents[0] / "desktopgui.ui")
-    self._device = clientapi.ClientAPI()
+    self._client_handler = client_handler
+    self._window = uic.loadUi(pathlib.Path(__file__).parents[0] / "client.ui")
     self._grab_bbox = None
     self._is_streaming = False
-
+    self._preview_img = None
     self._last_preview_bytes = None
     self._screen_preview_timer = QtCore.QTimer(self)
     self._screen_preview_timer.timeout.connect(self._update_screen_preview)
@@ -70,6 +68,7 @@ class DesktopApp(QtWidgets.QMainWindow):
     self._window.push_button_any.clicked.connect(lambda : self._set_screen_area(resizable=True, fixed_ratio=False))
 
     self._window.button_go_live_snapshot.clicked.connect(self._button_go_live_snapshot_clicked)
+    self._window.button_go_live_stream.clicked.connect(self._button_go_live_stream_clicked)
     # self._window.button_take_image.clicked.connect(self._take_image)
     # self._window.button_live.clicked.connect(self._toggle_stream)
 
@@ -101,48 +100,39 @@ class DesktopApp(QtWidgets.QMainWindow):
 
     except:
       # If something fails, deal with it.
+      self._show()
       self._grab_bbox = None
       self._screen_preview_timer.stop
       # self._kill_update_preview_thread()
 
   def _update_enabledness(self):
     self._window.button_go_live_snapshot.setEnabled(self._grab_bbox is not None)
+    self._window.button_go_live_stream.setEnabled(self._grab_bbox is not None)
     # self._window.button_take_video.setEnabled(False)
     # self._window.button_live.setEnabled(self._grab_bbox is not None)
     pass
 
-  # def _take_image(self):
-  #   self._is_streaming = False
-  #   self._grab_image()
-  #   arr = io.BytesIO()
-  #   self._img.save(arr, format="gif")
-  #   print(arr.getvalue())
-  #   self._device.set_slot(0, arr.getvalue())
-
   def _button_go_live_snapshot_clicked(self):
-    # print('_button_go_live_snapshot_clicked')
-    if self._last_preview_bytes is not None:
-      self._device.set_slot(0, self._last_preview_bytes)
-    # mem = QtCore.QBuffer(self)
-    
-    # self._window.label_screen_preview.pixmap().save(mem, "GIF")
-    # print(bytes(mem.data()).decode())
-    # self._device.set_slot(0, bytes(mem.data()).decode())
-    # qimage = pixmap.toImage()
-    # pass
+    self._client_handler.process_go_live_screenshot()
 
+  def _button_go_live_stream_clicked(self):
+    self._client_handler.process_go_live_stream()
 
   def _update_screen_preview(self):
+    # Take an image.
     assert self._grab_bbox is not None
-    self._img = ImageGrab.grab(bbox=self._grab_bbox)
-    if self._img.width != MATRIX_WIDTH or self._img.height != MATRIX_HEIGHT:
+    new_preview_img = ImageGrab.grab(bbox=self._grab_bbox)
+    if new_preview_img.width != MATRIX_WIDTH or new_preview_img.height != MATRIX_HEIGHT:
       resample_method = self._window.combo_resample_method.itemData(self._window.combo_resample_method.currentIndex())
-      self._img = self._img.resize((MATRIX_WIDTH, MATRIX_HEIGHT), resample=resample_method)
+      new_preview_img = new_preview_img.resize((MATRIX_WIDTH, MATRIX_HEIGHT), resample=resample_method)
 
-    arr = io.BytesIO()
-    self._img.save(arr, format="gif")
-    self._last_preview_bytes = arr.getvalue()
-    self._qt_img = PIL_to_qimage(self._img)
+    self._preview_img = new_preview_img
+
+    # Call handler.
+    self._client_handler.process_screen_image(self._preview_img)
+
+    # Update preview widget.
+    self._qt_img = PIL_to_qimage(self._preview_img)
     self._qt_pix = QtGui.QPixmap.fromImage(self._qt_img, QtCore.Qt.ImageConversionFlag.AutoColor)
     self._window.label_screen_preview.setPixmap(self._qt_pix)
 
@@ -156,9 +146,10 @@ class DesktopApp(QtWidgets.QMainWindow):
     QtWidgets.QApplication.exec()
 
 def main(width=128, height=128):
+  import clientlogic
   app = QtWidgets.QApplication(sys.argv)
-  window = DesktopApp()
-  window.show()
+  client_logic = clientlogic.ClientLogic()
+  client_app = ClientApp(client_logic)
   app.exec()
 
 
